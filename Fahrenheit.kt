1eaa4dae438444f10a9d649ea6a7628fabd068f1
zulalc <83912055+zulalc@users.noreply.github.com>
2023-08-09 19:38:23 +0300

    Add files via upload

---
+++ Fahrenheit.kt
@@ -0,0 +1,7 @@
+package com.info.homeworks
+
+class Fahrenheit {
+    fun fahrenConverter(number:Double):Double{
+        return number*1.8 +32
+    }
+}
---
+++ FahrenheitMain.kt
@@ -0,0 +1,8 @@
+package com.info.homeworks
+
+fun main() {
+    val f = Fahrenheit()
+
+    val conversion = f.fahrenConverter(36.0)
+    println("Fahrenheit: $conversion")
+}
